98afa9f75d1407a37871c4418c887b3bd08b5121
brightmanforever022 <brightmanforever@outlook.com>
2017-03-14 21:34:16 +0100

    change options

--- resources/views/frontend/search/search.blade.php
+++ resources/views/frontend/search/search.blade.php
@@ -72,21 +72,76 @@
       <p class="more_options">More Options +</p>
       <div class="more_search_options">
         <p class="close_options">Close Options x</p>
-        <div class="select-icon">
-          {{ Form::select('bedroom', array('' => 'Bedroom', '1' => '1', '2' => '2','3' => '3','4' => '4','5' => '5','6' => '6', '7' => '7',)) }}
-        </div>
-        <div class="select-icon">
-          {{ Form::select('bathroom', array('' => 'Bathroom', '1' => '1', '2' => '2','3' => '3','4' => '4','5' => '5','6' => '6', '7' => '7',)) }}
-        </div>
-        <div class="select-icon">
-          {{ Form::select('bed', array('' => 'Beds', '1' => '1', '2' => '2','3' => '3','4' => '4','5' => '5','6' => '6', '7' => '7',)) }}
-        </div>
-        <div class="select-icon">
-          {{ Form::select('lining_space', array('' => 'Living Space m2', '32' => '32', '45' => '45','55' => '55','75' => '75', '100' => '100',)) }}
-        </div>
-        <div class="select-icon">
-          {{ Form::select('property_type_id', array('' => 'Property Type', '1' => '1', '2' => '2','3' => '3','4' => '4','5' => '5','6' => '6', '7' => '7',)) }}
-        </div>
+        <ul>
+          <li class="more_search_options_list">
+            <ul>
+              <li>
+                <div class="select-icon">
+                  {{ Form::select('bedroom', array('' => 'Bedroom', '1' => '1', '2' => '2','3' => '3','4' => '4','5' => '5','6' => '6', '7' => '7',)) }}
+                </div>
+              </li>
+              <li>
+                <div class="select-icon">
+                  {{ Form::select('bathroom', array('' => 'Bathroom', '1' => '1', '2' => '2','3' => '3','4' => '4','5' => '5','6' => '6', '7' => '7',)) }}
+                </div>
+              </li>
+              <li>
+                <div class="select-icon">
+                  {{ Form::select('bed', array('' => 'Beds', '1' => '1', '2' => '2','3' => '3','4' => '4','5' => '5','6' => '6', '7' => '7',)) }}
+                </div>
+              </li>
+              <li>
+                <div class="select-icon">
+                  {{ Form::select('lining_space', array('' => 'Living Space m2', '32' => '32', '45' => '45','55' => '55','75' => '75', '100' => '100',)) }}
+                </div>
+              </li>
+              <li>
+                <div class="select-icon">
+                  {{ Form::select('property_type_id', array('' => 'Property Type', '1' => '1', '2' => '2','3' => '3','4' => '4','5' => '5','6' => '6', '7' => '7',)) }}
+                </div>
+              </li>
+            </ul>
+          </li>
+          <li class="more_search_options_list">
+            <ul>
+              <li><label for="non_smoking"><input type="checkbox" id="non_smoking" name="non_smoking" />Non Smoking</label></li>
+              <li><label for="tv"><input type="checkbox" id="tv" name="tv" />TV</label></li>
+              <li><label for="w_lan"><input type="checkbox" id="w_lan" name="w_lan" />W-LAN</label></li>
+              <li><label for="bed_linen"><input type="checkbox" id="bed_linen" name="bed_linen" />Bed linen</label></li>
+              <li><label for="work_desk"><input type="checkbox" id="work_desk" name="work_desk" />Work desk</label></li>
+              <li><label for="towels"><input type="checkbox" id="towels" name="towels" />Towels</label></li>
+              <li><label for="pods"><input type="checkbox" id="pods" name="pods" />Pods, dishes</label></li>
+            </ul>
+          </li>
+          <li class="more_search_options_list">
+            <ul>
+              <li><label for="hairdryer"><input type="checkbox" id="hairdryer" name="hairdryer" />Hairdryer</label></li>
+              <li><label for="iron"><input type="checkbox" id="iron" name="iron" />Iron</label></li>
+              <li><label for="phone"><input type="checkbox" id="phone" name="phone" />Phone</label></li>
+              <li><label for="safe"><input type="checkbox" id="safe" name="safe" />Safe</label></li>
+              <li><label for="washing_machine"><input type="checkbox" id="washing_machine" name="washing_machine" />Washing machine</label></li>
+              <li><label for="fridge"><input type="checkbox" id="fridge" name="fridge" />Fridge</label></li>
+              <li><label for="dishwasher"><input type="checkbox" id="dishwasher" name="dishwasher" />Dishwasher</label></li>
+            </ul>
+          </li>
+          <li class="more_search_options_list">
+            <ul>
+              <li><label for="wardrobe"><input type="checkbox" id="wardrobe" name="wardrobe" />Built-in wardrobe</label></li>
+              <li><label for="parking"><input type="checkbox" id="parking" name="parking" />Parking</label></li>
+              <li><label for="concierge"><input type="checkbox" id="concierge" name="concierge" />Concierge</label></li>
+              <li><label for="laundry"><input type="checkbox" id="laundry" name="laundry" />Laundry</label></li>
+            </ul>
+          </li>
+          <li class="more_search_options_list">
+            <ul>
+              <li><label for="wheelchair_access"><input type="checkbox" id="wheelchair_access" name="wheelchair_access" />Wheelchair access</label></li>
+              <li><label for="fitness_center"><input type="checkbox" id="fitness_center" name="fitness_center" />Fitness Center</label></li>
+              <li><label for="elevator"><input type="checkbox" id="elevator" name="elevator" />Elevator</label></li>
+              <li><label for="cellar"><input type="checkbox" id="cellar" name="cellar" />Cellar</label></li>
+            </ul>
+          </li>
+          <div class="clearfix"></div>
+        </ul>
       </div>
     </div>
     <!--/form --> 
