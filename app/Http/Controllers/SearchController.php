de90dbffdb7e6e337daa051bb82729ef8ed55b18
brightmanforever022 <brightmanforever@outlook.com>
2017-03-20 19:54:57 +0100

    more search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -601,7 +601,7 @@ class SearchController extends Controller
                 /**************NEW AVALIABLITY FUNCTIONALITY ENDS HERE*****************/ 
             }
             
-            return view('frontend.search.search')->with('porperties', $temp_properties)->with('city_where_met_location', $city_where_met)->with('some_place', Input::get('city_where_met_search'))->with('price', Input::get('price'))->with('bedroom', Input::get('bedroom'))->with('start_date', $pass)->with('radius', 0)->with('end_date', $pass_end);;
+            return view('frontend.search.search')->with('porperties', $porperties)->with('city_where_met_location', $city_where_met)->with('some_place', Input::get('city_where_met_search'))->with('price', Input::get('price'))->with('bedroom', Input::get('bedroom'))->with('start_date', $pass)->with('radius', 0)->with('end_date', $pass_end);;
         }else
         {
             \Session::flash('message1', 'We could not find any locations with the name "". Please check the name and search again.'); 
