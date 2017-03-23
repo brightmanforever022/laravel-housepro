24a4e680e9590f0cdae3b21650d587a09a80fcd8
brightmanforever022 <brightmanforever@outlook.com>
2017-03-23 15:37:17 +0100

    arrange search controller

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -649,7 +649,7 @@ class SearchController extends Controller
                 }
                 /**************NEW AVALIABLITY FUNCTIONALITY ENDS HERE*****************/ 
             }
-            // print_r($porperties);exit;
+            
             // When $porperties has list, appends 'get' -> parameters into $porperties variable.
             if(count($porperties) > 0){
               return view('frontend.search.search')->with('porperties', $porperties->appends(Input::except('page')))->with('city_where_met_location', $city_where_met)->with('some_place', Input::get('city_where_met_search'))->with('price', Input::get('price'))->with('bedroom', Input::get('bedroom'))->with('start_date', $pass)->with('radius', 0)->with('end_date', $pass_end);
@@ -669,7 +669,7 @@ class SearchController extends Controller
       if($more_keys['bedroom1'] != null) {
         $last_result = $last_result->where('bedroom', '=', $more_keys['bedroom1'])->get();
       }
-      
+
       if($more_keys['bathroom'] != null){
         $last_result = $last_result->where('bathroom', '=', $more_keys['bathroom'])->get();
       }
